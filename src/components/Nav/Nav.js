edfd2614166d01bbdeda8f56d6e4ff207a672a08
dutraneto <dutraneto@me.com>
2020-10-14 18:54:04 -0400

    Add react router to navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 import './Nav.scss'
 
 import Logo from '../Logo/Logo'
@@ -7,76 +8,69 @@ import { FiSearch, FiPackage, FiHeart, FiLogOut } from 'react-icons/fi'
 import { FaRegUserCircle } from 'react-icons/fa'
 import { RiShoppingBasket2Line } from 'react-icons/ri'
 
-const Nav = (props) => (
-    <nav>
-        <ul className='nav'>
-            <li className='nav__item nav__item_active'>
-                <a href='./' className='nav__link'>
-                    inicio
-                </a>
-            </li>
-            <li className='nav__item'>
-                <a href='./' className='nav__link'>
-                    blog
-                </a>
-            </li>
-            <li className='nav__item'>
-                <a href='./' className='nav__link'>
-                    loja
-                </a>
-            </li>
-            <li className='nav__item'>
-                <a href='./' className='nav__link'>
-                    podcasts
-                </a>
-            </li>
-            <li className='nav__item'>
-                <a href='./' className='nav__link'>
-                    cursos
-                </a>
-            </li>
-            <li className='nav__item'>
-                <a href='./' className='nav__link'>
-                    contato
-                </a>
-            </li>
-        </ul>
+const Nav = (props) => {
+    const navItems = [
+        { name: 'início' },
+        { name: 'blog' },
+        { name: 'loja' },
+        { name: 'podcasts' },
+        { name: 'cursos' },
+        { name: 'contato' },
+    ]
 
-        <Logo className='logo' />
+    const navList = navItems.map((item, index) => {
+        return (
+            <li key={index} className='nav__item'>
+                <NavLink to={item.name} className='nav__link' activeClassName='active'>
+                    {item.name}
+                </NavLink>
+            </li>
+        )
+    })
 
-        <ul className='nav nav__right'>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <FiSearch />
-                </a>
-            </li>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <FaRegUserCircle />
-                </a>
-            </li>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <FiPackage />
-                </a>
-            </li>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <FiHeart />
-                </a>
-            </li>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <RiShoppingBasket2Line />
-                </a>
-            </li>
-            <li className='nav__item nav__item--icon'>
-                <a href='./' className='nav__link'>
-                    <FiLogOut />
-                </a>
-            </li>
-        </ul>
-    </nav>
-)
+    return (
+        <nav>
+            <ul className='nav'>
+                <Router>{navList}</Router>
+            </ul>
+
+            <Logo className='logo' />
+
+            <ul className='nav nav__right'>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <FiSearch />
+                    </a>
+                </li>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <FaRegUserCircle />
+                    </a>
+                </li>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <FiPackage />
+                    </a>
+                </li>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <FiHeart />
+                    </a>
+                </li>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <span className='nav__link--count'>99</span>
+                        <RiShoppingBasket2Line />
+                    </a>
+                </li>
+                <li className='nav__item nav__item--icon'>
+                    <a href='./' className='nav__link'>
+                        <FiLogOut />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
 
 export default Nav
