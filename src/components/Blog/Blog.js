12d5b25f5d000849c327adcd05c577d94f08fe16
dutraneto <dutraneto@me.com>
2020-10-20 17:56:09 -0400

    Done blog styles

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import './Blog.scss'
+import data from './data'
 
 import HorizontalRule from '../HorizontalRule/HorizontalRule'
 import Button from '../Button/Button'
 
 export default function Blog(props) {
+    const listOfPosts = data.map(({ id, src, title, subtitle, author, highlights }) => {
+        return (
+            <li key={id} className={`posts__item posts__item--${id}`}>
+                <a href='./' className='posts__item-link'>
+                    <picture className='posts__item-pic'>
+                        <img className='posts__item-img' src={src} alt={title} />
+                        <figcaption className={`posts__item-title posts__item-title--${id}`}>
+                            {title}
+                        </figcaption>
+
+                        <div className={`posts__item-box posts__item-box--${id}`}>
+                            <span className={`posts__item-subtitle posts__item-subtitle--${id}`}>
+                                {subtitle}
+                            </span>
+                            <span className={`posts__item-author posts__item-author--${id}`}>
+                                {author}
+                            </span>
+                        </div>
+                        <span className={`posts__item-highlights posts__item-highlights--${id}`}>
+                            {highlights}
+                        </span>
+                    </picture>
+                </a>
+            </li>
+        )
+    })
     return (
-        <section className='blog'>
+        <section className='posts'>
             <HorizontalRule>Blog</HorizontalRule>
+            <ul className='posts__list'>
+                {listOfPosts}
+                <li className='posts__item posts__item-form'></li>
+            </ul>
             <Button>Veja todos os posts ⟶</Button>
         </section>
     )
---
+++ src/components/Blog/data.js
@@ -0,0 +1,42 @@
+const images = [
+    {
+        id: 1,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603127597/bigbang/Rectangle_6_xmkf2s.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        subtitle: 'VIAGEM',
+        author: 'Fulano de Tal',
+        highlights: '',
+    },
+    {
+        id: 2,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603127596/bigbang/Rectangle_112_rnql5c.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        subtitle: 'DESENVOLVIMENTO PESSOAL',
+        author: 'Fulano de Tal',
+        highlights: '',
+    },
+    {
+        id: 3,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603127597/bigbang/Rectangle_233_fmyez7.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        subtitle: 'VIAGEM',
+        author: '',
+        highlights:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...',
+    },
+    {
+        id: 4,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603127596/bigbang/Rectangle_112_rnql5c.jpg',
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        subtitle: 'VIAGEM',
+        author: '',
+        highlights:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna...',
+    },
+]
+
+export default images
