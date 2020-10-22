6f0c5845db2f5335978e4fe13d5829bca753c83b
dutraneto <dutraneto@me.com>
2020-10-22 00:35:15 -0400

    Add data to section shop

---
+++ src/components/Shop/data.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+        id: 1,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603340867/bigbang/Rectangle16_pvgc8q.jpg',
+        title: 'Ovo de Páscoa de Paçoca',
+        subtitle: 'Lorem ipsum dolor',
+        price: 54.0,
+    },
+    {
+        id: 2,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603340865/bigbang/Rectangle17_ddvh5z.jpg',
+        title: 'Ovo de Páscoa de Paçoca',
+        subtitle: 'Lorem ipsum dolor',
+        price: 32.0,
+    },
+    {
+        id: 3,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603340865/bigbang/Rectangle18_eat0fw.jpg',
+        title: 'Ovo de Páscoa de Paçoca',
+        subtitle: 'Lorem ipsum dolor',
+        price: 49.0,
+    },
+    {
+        id: 4,
+        src:
+            'https://res.cloudinary.com/duim1pyqp/image/upload/v1603340865/bigbang/Rectangle19_d5ulai.jpg',
+        title: 'Ovo de Páscoa de Paçoca',
+        subtitle: 'Lorem ipsum dolor',
+        price: 59.0,
+    },
+]
+
+export default products
